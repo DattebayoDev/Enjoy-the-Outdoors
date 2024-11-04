25970d315ce98ec2e8d9a1575622d0f064bc5288
angcsherpa01@gmail.com <angcsherpa01@gmail.com>
2024-11-04 12:07:06 -0500

    finished refactoring national functions

--- main.js
+++ main.js
@@ -5,23 +5,24 @@ function nationalPark_selectDropDown() {
   const allRadios = document.querySelectorAll("input[type='radio']");
   allRadios.forEach((radio) => {
     radio.addEventListener("click", () => {
-      resetDropdown()
-      displayDropDown(populateDropDown(radio, selectedDropdown(radio), selectedArray(radio)))
-
+      resetDropdown();
+      displayDropDown(
+        populateDropDown(radio, selectedDropdown(radio), selectedArray(radio))
+      );
     });
-  })
+  });
 }
 
 function selectedDropdown(radio) {
-  const selectedDropdown = (radio.id === "location-radio") ? "location-DropDown" : "parkType-DropDown"
-  return selectedDropdown 
+  const selectedDropdown =
+    radio.id === "location-radio" ? "location-DropDown" : "parkType-DropDown";
+  return selectedDropdown;
 }
-
 function selectedArray(radio) {
-  const selectedArray = (radio.id === "location-radio") ? locationsArray : parkTypesArray
-  return selectedArray
+  const selectedArray =
+    radio.id === "location-radio" ? locationsArray : parkTypesArray;
+  return selectedArray;
 }
-
 function populateDropDown(radioInput, dropDownID, array) {
   const dropDown = document.getElementById(dropDownID);
   //dropDown.length = 1;
@@ -30,31 +31,34 @@ function populateDropDown(radioInput, dropDownID, array) {
     option.textContent = element;
     dropDown.appendChild(option);
   });
-  return { radioInput, dropDown }
+  return { radioInput, dropDown };
 }
-function resetDropdown(){
+function resetDropdown() {
   const allSelect = document.querySelectorAll("select");
   allSelect.forEach((select) => {
-      select.classList.add("hide-display")
-  })
+    select.classList.add("hide-display");
+  });
 }
-
 function displayDropDown(obj) {
-  radioInput =  obj.radioInput
-  dropDown =  obj.dropDown
-  const activeDropdown = (radioInput.checked ? dropDown.classList.remove("hide-display"): dropDown.classList.add("hide-display"))
+  radioInput = obj.radioInput;
+  dropDown = obj.dropDown;
+  const activeDropdown = radioInput.checked
+    ? dropDown.classList.remove("hide-display")
+    : dropDown.classList.add("hide-display");
   capturingInput(dropDown);
 }
-  
+
 function capturingInput(dropDownElement) {
   dropDownElement.addEventListener("change", () => {
     const selectedOption = dropDownElement.value;
+    createTableHeaders(eraseTableData());
     parsingArray(dropDownElement, selectedOption);
   });
 }
 
-function parsingArray(dropDownElement, selectedOption) {
-  eraseTableData();
+function eraseTableData() {
+  const tableBody = document.querySelector("tbody");
+  tableBody.textContent = "";
   tempTableHeadersObj = {
     LocationName: "Name",
     Address: "Address",
@@ -64,41 +68,38 @@ function parsingArray(dropDownElement, selectedOption) {
     Phone: "Phone",
     Visit: "Visit",
   };
-  let tableHeadersArray = createTableHeaders(tempTableHeadersObj);
-  if (dropDownElement.id === "location-DropDown") {
-    nationalParksArray.forEach((park) => {
-      if (selectedOption === park.State) {
-        const newDiv = document.createElement("div");
-        newDiv.textContent = JSON.stringify(park);
-        createTableData(tableHeadersArray, park);
-      }
-    });
-  } else {
-    nationalParksArray.forEach((park) => {
-      console.log(typeof park.LocationName);
-      if (park.LocationName.includes(selectedOption)) {
-        const newDiv = document.createElement("div");
-        newDiv.textContent = JSON.stringify(park);
-        createTableData(tableHeadersArray, park);
-      }
-    });
-  }
+  return tempTableHeadersObj;
 }
 
-// i am using the array to make sure that parks with no visit website have an empty cell
 function createTableHeaders(obj) {
   const values = Object.values(obj);
   const tableHeader = document.querySelector("thead");
   tableHeader.textContent = "";
-  const tableHeadersArray = [];
 
   values.forEach((value) => {
     const header = document.createElement("th");
     header.textContent = value;
     tableHeader.appendChild(header);
-    tableHeadersArray.push(value);
   });
-  return obj;
+  return values;
+}
+
+function parsingArray(dropDownElement, selectedOption) {
+  eraseTableData();
+  let tableHeadersArray = eraseTableData();
+  if (dropDownElement.id === "location-DropDown") {
+    nationalParksArray.forEach((state) => {
+      if (selectedOption === state.State) {
+        createTableData(tableHeadersArray, state);
+      }
+    });
+  } else {
+    nationalParksArray.forEach((park) => {
+      if (park.LocationName.includes(selectedOption)) {
+        createTableData(tableHeadersArray, park);
+      }
+    });
+  }
 }
 
 function createTableData(tableHeadersArray, obj) {
@@ -108,19 +109,10 @@ function createTableData(tableHeadersArray, obj) {
 
   tempArray.forEach((header) => {
     const tableDataCell = document.createElement("td");
-    let tempVariable = Object.hasOwn(obj, header) ? obj[header] : "";
-    tableDataCell.textContent =
-      typeof tempVariable === "object"
-        ? JSON.stringify(tempVariable)
-        : tempVariable;
+    tableDataCell.textContent = Object.hasOwn(obj, header) ? obj[header] : "";
     row.appendChild(tableDataCell);
     tableBody.appendChild(row);
   });
-}
-
-function eraseTableData() {
-  const tableBody = document.querySelector("tbody");
-  tableBody.textContent = "";
 }
 
 //#endregion
